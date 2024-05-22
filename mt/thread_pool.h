e2e897904a89aac053449b5cae79fd5afe04a10e
Vladislav Belousov <vladbelousov@yahoo.com>
2024-05-22 15:29:03 +0100

    Rename ThreadPool to other in a ThreadPool constructor

--- mt/thread_pool.h
+++ mt/thread_pool.h
@@ -10,7 +10,7 @@ namespace kiwi {
     explicit ThreadPool(std::size_t num_threads);
     ~ThreadPool();
 
-    ThreadPool(const Job& ThreadPool) = delete;
+    ThreadPool(const Job& other) = delete;
     ThreadPool(ThreadPool&& other) = delete;
     ThreadPool& operator=(const ThreadPool& other) = delete;
     ThreadPool& operator=(ThreadPool&& other) = delete;
