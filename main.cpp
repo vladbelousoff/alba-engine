a9492f44592ccd41662964eb9838dbe106d3c83c
Vladislav Belousov <vladbelousov@yahoo.com>
2024-06-16 19:31:11 +0100

    Texture loading

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@
 #include "shader.h"
 #include "time/scope_timer.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "third_party/stb/stb_image.h"
+
 namespace alba {
   static void handle_glfw_error(int error, const char* description)
   {
@@ -23,82 +26,49 @@ namespace alba {
   static std::string default_shader_vert =
       "#version 330 core\n"
       "layout (location = 0) in vec3 a_position;\n"
+      "layout (location = 1) in vec3 a_color;\n"
+      "layout (location = 2) in vec2 a_texcoord;\n"
       "uniform mat4 u_model;\n"
       "uniform mat4 u_view;\n"
       "uniform mat4 u_projection;\n"
+      "out vec3 color;\n"
+      "out vec2 texcoord;\n"
       "void main() {\n"
       "  gl_Position = u_projection * u_view * u_model * vec4(a_position, 1.0f);\n"
+      "  color = a_color;\n"
+      "  texcoord = a_texcoord;\n"
       "}\n";
 
   static std::string default_shader_frag =
       "#version 330 core\n"
-      "in vec2 textcoord;\n"
       "out vec4 frag_color;\n"
-      "uniform float u_time;\n"
+      "in vec3 color;\n"
+      "in vec2 texcoord;\n"
+      "uniform sampler2D u_texture;\n"
       "void main() {\n"
-      "  float blue = (sin(u_time) + 1.0) * 0.5;\n"
-      "  frag_color = vec4(1.0, 0.5, blue, 1.0);\n"
+      "  frag_color = texture(u_texture, texcoord);\n"
       "}\n";
 
-  struct Vertex
-  {
-    float x, y, z;
+  static float vertices[] = {
+    // positions // colors // texture coords
+    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
   };
 
-  std::vector<Vertex> generate_grid_vertices(float size, int n)
-  {
-    std::vector<Vertex> grid_vertices;
-
-    // Calculate the step size between each grid cell
-    float step = size / (float)n;
-
-    // Generate vertices
-    for (int i = 0; i <= n; ++i) {
-      for (int j = 0; j <= n; ++j) {
-        Vertex v{};
-        v.x = -size / 2.0f + (float)j * step;
-        v.y = 0.0f;
-        v.z = -size / 2.0f + (float)i * step;
-        grid_vertices.push_back(v);
-      }
-    }
-
-    return grid_vertices;
-  }
-
-  std::vector<GLuint> generate_grid_indices(int n)
-  {
-    std::vector<GLuint> grid_indices;
-
-    // Generate indices for each quad
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        GLuint top_left = i * (n + 1) + j;
-        GLuint top_right = top_left + 1;
-        GLuint bottom_left = (i + 1) * (n + 1) + j;
-        GLuint bottom_right = bottom_left + 1;
-
-        // First triangle of the quad
-        grid_indices.push_back(top_left);
-        grid_indices.push_back(bottom_left);
-        grid_indices.push_back(top_right);
-
-        // Second triangle of the quad
-        grid_indices.push_back(top_right);
-        grid_indices.push_back(bottom_left);
-        grid_indices.push_back(bottom_right);
-      }
-    }
-
-    return grid_indices;
-  }
+  unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
 
 } // namespace alba
 
 namespace alba {
   namespace global {
     static float delta_time = 0.f;
-  }
+    static std::filesystem::path root_path;
+  } // namespace global
 
   auto get_delta_time() -> float
   {
@@ -113,8 +83,8 @@ namespace alba {
 
 struct
 {
-  float distance_to_origin{ 3.45f };
-  float phi{ 0.6f }, theta{ 2.7f };
+  float distance_to_origin{ 5.6f };
+  float phi{ 4.2f }, theta{ 1.7f };
 } camera;
 
 // Function to handle key presses
@@ -131,7 +101,10 @@ int main(int argc, char* argv[])
   argv = app.ensure_utf8(argv);
 
   CLI::Option* run_tests = app.add_flag("--run-tests");
+  app.add_option("--root", alba::global::root_path);
   CLI11_PARSE(app, argc, argv)
+
+  spdlog::info("Root: {}", absolute(alba::global::root_path).string());
 
   if (run_tests->count()) {
     testing::InitGoogleTest();
@@ -198,21 +171,52 @@ int main(int argc, char* argv[])
   // Bind VAO
   glBindVertexArray(vao);
 
-  constexpr int side_n = 32;
-  std::vector<alba::Vertex> vertices = alba::generate_grid_vertices(1.f, side_n);
-  std::vector<GLuint> indices = alba::generate_grid_indices(side_n);
+  // constexpr int side_n = 32;
+  // std::vector<alba::Vertex> vertices = alba::generate_grid_vertices(1.f, side_n);
+  // std::vector<GLuint> indices = alba::generate_grid_indices(side_n);
 
   // Bind VBO and copy vertices data
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)(vertices.size() * sizeof(alba::Vertex)), vertices.data(), GL_STATIC_DRAW);
+  // glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)(vertices.size() * sizeof(alba::Vertex)), vertices.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(alba::vertices), alba::vertices, GL_STATIC_DRAW);
 
   // Bind EBO and copy indices data
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizeiptr)(indices.size() * sizeof(GLuint)), indices.data(), GL_STATIC_DRAW);
+  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizeiptr)(indices.size() * sizeof(GLuint)), indices.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(alba::indices), alba::indices, GL_STATIC_DRAW);
 
-  // Set vertex attribute pointers
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(alba::Vertex), (GLvoid*)nullptr);
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)nullptr);
   glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  spdlog::info("Loading textures...");
+
+  std::uint32_t texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture
+  int width, height, nr_channels;
+  std::filesystem::path texture_path = alba::global::root_path / std::filesystem::path{ "data/wall.jpg" };
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char* data = stbi_load(texture_path.string().c_str(), &width, &height, &nr_channels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+  } else {
+    spdlog::error("Failed to load texture");
+  }
 
   // Unbind VAO, VBO, and EBO
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -230,8 +234,8 @@ int main(int argc, char* argv[])
 
   // Needed before setting uniforms
   alba::ShaderManager::use_program(prog);
-
   alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_projection" }, proj);
+  alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_texture" }, 0);
 
   auto transform = glm::mat4(1.0f);
 
@@ -260,13 +264,13 @@ int main(int argc, char* argv[])
   while (!glfwWindowShouldClose(window)) {
     alba::ScopeTimer scope_timer{ alba::global::delta_time };
 
-    thread_pool.submit_job(job1);
-    thread_pool.submit_job(job2);
-    thread_pool.submit_job(job3);
+    // thread_pool.submit_job(job1);
+    // thread_pool.submit_job(job2);
+    // thread_pool.submit_job(job3);
 
     // Wait for all jobs to be done
-    thread_pool.wait_all();
-    thread_pool.reset();
+    // thread_pool.wait_all();
+    // thread_pool.reset();
 
     float x = camera.distance_to_origin * glm::sin(camera.phi) * glm::cos(camera.theta);
     float y = camera.distance_to_origin * glm::cos(camera.phi);
@@ -274,6 +278,8 @@ int main(int argc, char* argv[])
 
     // Calculate view matrix
     glm::mat4 view = glm::lookAt(glm::vec3(x, y, z), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+    alba::ShaderManager::use_program(prog);
 
     // Update uniform
     alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_view" }, view);
@@ -286,18 +292,19 @@ int main(int argc, char* argv[])
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glCullFace(GL_BACK);
 
-    alba::ShaderManager::use_program(prog);
-    glBindVertexArray(vao);
-
-    alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_time" }, (float)scope_timer.get_start());
+    // alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_time" }, (float)scope_timer.get_start());
 
     const auto model_y = (float)glm::sin(scope_timer.get_start());
     auto new_transform = glm::translate(transform, glm::vec3(0.f, model_y, 0.f));
     alba::ShaderManager::set_uniform(prog, alba::StringID{ "u_model" }, new_transform);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glDrawElements(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, nullptr);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glDrawElements(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, nullptr);
     glBindVertexArray(0);
 
     // Start the Dear ImGui frame
